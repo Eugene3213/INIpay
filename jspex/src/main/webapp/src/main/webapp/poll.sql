34afdd6ff181d251f50abc7a7248c4f7bb0bfc1e
Eugene3213 <106224826+Eugene3213@users.noreply.github.com>
2022-08-18 18:23:40 +0900

    Delete jspex/src/main/webapp/src/main/webapp directory

--- jspex/src/main/webapp/src/main/webapp/poll.sql
+++
@@ -1,4 +0,0 @@
-insert into poll values ('0','이순신','lee','01055523334','3','2022-08-16 15:13:22');
-insert into poll values ('0','유관순','yu','01055523213','2','2022-08-16 17:11:22');
-
-select * from poll order by idx desc;
